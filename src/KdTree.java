9b94713abd8ba49aadd2aa119faaac9e31e8d93a
runningiris <irisrunning@foxmail.com>
2019-12-25 12:48:22 +0800

    Score: 97

--- src/KdTree.java
+++ src/KdTree.java
@@ -15,6 +15,7 @@ public class KdTree {
         private RectHV rect;
         private Node lb;
         private Node rt;
+        private boolean flag;
     }
 
     final private Node root;
@@ -27,38 +28,18 @@ public class KdTree {
     public KdTree() {
         root = new Node();
         root.rect = new RectHV(0, 0, 1, 1);
+        root.flag = true;
     }
 
-    // 水平插入点 p
-    private void insertHorizontal(Point2D p, Node node) {
-        if (node.p == null) {
-            node.p = p;
-            node.lb = new Node();
-            node.lb.rect = new RectHV(node.rect.xmin(), node.rect.ymin(), p.x(), node.rect.ymax());
-            node.rt = new Node();
-            node.rt.rect = new RectHV(p.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax());
-        }
-    }
-
-    // 竖直插入点 p
-    private void insertVertical(Point2D p, Node node) {
-        if (node.p == null) {
-            node.p = p;
-            node.lb = new Node();
-            node.lb.rect = new RectHV(node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), p.y());
-            node.rt = new Node();
-            node.rt.rect = new RectHV(node.rect.xmin(), p.y(), node.rect.xmax(), node.rect.ymax());
-        }
-    }
-
-    private void dfsDraw(Node node, boolean flag) {
+    private void dfsDraw(Node node) {
         if (node.p != null) {
-            if (flag) {
+            if (node.flag) {
                 StdDraw.setPenColor(Color.red);
                 double x = node.p.x();
                 double ymin = node.rect.ymin();
                 double ymax = node.rect.ymax();
                 StdDraw.line(x, ymin, x, ymax);
+
             } else {
                 StdDraw.setPenColor(Color.blue);
                 double y = node.p.y();
@@ -70,9 +51,54 @@ public class KdTree {
             StdDraw.setPenColor(Color.black);
             node.p.draw();
 
-            if (node.lb.p != null) dfsDraw(node.lb, !flag);
-            if (node.rt.p != null) dfsDraw(node.rt, !flag);
+            if (node.lb.p != null) dfsDraw(node.lb);
+            if (node.rt.p != null) dfsDraw(node.rt);
         }
+    }
+
+    private void insert(Node node, Point2D p) {
+        node.p = p;
+        node.lb = new Node();
+        node.rt = new Node();
+
+        node.lb.flag = !node.flag;
+        node.rt.flag = !node.flag;
+
+        double lbXMin;
+        double lbYMin;
+        double lbXMax;
+        double lbYMax;
+        double rtXMin;
+        double rtYMin;
+        double rtXMax;
+        double rtYMax;
+
+        if (node.flag) { // 竖线分开矩形
+            lbXMin = node.rect.xmin();
+            lbXMax = p.x();
+            lbYMin = node.rect.ymin();
+            lbYMax = node.rect.ymax();
+
+            rtXMin = p.x();
+            rtXMax = node.rect.xmax();
+            rtYMin = node.rect.ymin();
+            rtYMax = node.rect.ymax();
+
+        } else { // 横线分开矩形
+            lbXMin = node.rect.xmin();
+            lbXMax = node.rect.xmax();
+            lbYMin = node.rect.ymin();
+            lbYMax = p.y();
+
+            rtXMin = node.rect.xmin();
+            rtXMax = node.rect.xmax();
+            rtYMin = p.y();
+            rtYMax = node.rect.ymax();
+        }
+
+        node.lb.rect = new RectHV(lbXMin, lbYMin, lbXMax, lbYMax);
+        node.rt.rect = new RectHV(rtXMin, rtYMin, rtXMax, rtYMax);
+        length++;
     }
 
     private List<Point2D> dfsRange(Node node, RectHV rect) {
@@ -113,6 +139,9 @@ public class KdTree {
 
     private void dfsNearest(Node node, Point2D p) {
         if (node.p == null) return;
+
+        // StdOut.println("Searching: " + node.p.toString());
+
         if (node.p.distanceSquaredTo(p) == 0) {
             nearestDistance = 0;
             nearest = node.p;
@@ -126,13 +155,31 @@ public class KdTree {
             nearest = node.p;
         }
 
-        if (node.lb != null && nearestDistance > node.lb.rect.distanceSquaredTo(p)) {
-            dfsNearest(node.lb, p);
+        if (
+                (node.flag && p.x() > node.p.x()) /* 以竖线分割，并且 p 在竖线的右边 */
+                ||
+                (!node.flag && p.y() > node.p.y()) /* 以横线分割，并且 p 在横线的上边 */
+        ) {
+            // 先搜右上
+            if (node.rt != null && nearestDistance > node.rt.rect.distanceSquaredTo(p)) {
+                dfsNearest(node.rt, p);
+            }
+
+            if (node.lb != null && nearestDistance > node.lb.rect.distanceSquaredTo(p)) {
+                dfsNearest(node.lb, p);
+            }
+        } else {
+            // 先搜左下
+            if (node.lb != null && nearestDistance > node.lb.rect.distanceSquaredTo(p)) {
+                dfsNearest(node.lb, p);
+            }
+
+            if (node.rt != null && nearestDistance > node.rt.rect.distanceSquaredTo(p)) {
+                dfsNearest(node.rt, p);
+            }
         }
 
-        if (node.rt != null && nearestDistance > node.rt.rect.distanceSquaredTo(p)) {
-            dfsNearest(node.rt, p);
-        }
+
     }
 
     // is the set empty?
@@ -146,31 +193,20 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException("argument p is null in the insert method.");
-
-        boolean flag = true; // 是否以 x 轴作为分界线
-        Node node = root; // 当前节点
+        Node node = root;
 
         while (node.p != null) {
+            if (node.p.x() == p.x() && node.p.y() == p.y()) return;
 
-            if (node.p.x() == p.x() && node.p.y() == p.y()) return; // 如果存在已有节点 == p，则不插入
-
-            if (flag) {
-                node = p.x() <= node.p.x() ? node.lb : node.rt; // 以 x 为分界线，沿 x 轴的左右递归
+            if (node.flag) {
+                node = p.x() < node.p.x() ? node.lb : node.rt;
             } else {
-                node = p.y() <= node.p.y() ? node.lb : node.rt; // 以 y 为分界线，沿 y 轴的上下递归
+                node = p.y() < node.p.y() ? node.lb : node.rt;
             }
-
-            flag = !flag;
         }
 
-        // 插入点 p
-        if (flag) {
-            insertHorizontal(p, node);
-        } else {
-            insertVertical(p, node);
-        }
+        insert(node, p);
 
-        length++;
     }
 
 
@@ -186,8 +222,7 @@ public class KdTree {
 
     // draw all points to standard draw
     public void draw() {
-        dfsDraw(root, true);
-
+        dfsDraw(root);
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -223,9 +258,15 @@ public class KdTree {
             ps.insert(new Point2D(file.readDouble(), file.readDouble()));
         }
 
+        Point2D query = new Point2D(0.96, 0.74);
+        StdDraw.setPenColor(Color.PINK);
+        StdDraw.setPenRadius(0.01);
+        query.draw();
+        StdDraw.setPenRadius(0.001);
+
+        StdOut.println("Nearest: " + ps.nearest(query));
         StdOut.println(ps.size());
         ps.draw();
 
-        StdOut.println(ps.contains(new Point2D(0.7, 0.0)));
     }
 }
